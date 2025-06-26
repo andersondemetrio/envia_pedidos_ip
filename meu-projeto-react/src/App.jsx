de345aa0a702c852ef5832f3d6acd75faefa2ce0
andersondemetrio <anderson.debora@gmail.com>
2025-06-26 10:11:36 -0300

    Nomeando o filho

--- meu-projeto-react/src/App.jsx
+++ meu-projeto-react/src/App.jsx
@@ -1139,6 +1139,11 @@ const IntelipostOrderCreator = () => {
           )}
         </div>
       </div>
+    <footer className="mt-12 py-4 text-center text-green-800 text-sm bg-green-100 border-t border-green-300">
+  <p className="font-semibold">Desenvolvido por Demetrio & Deb © {new Date().getFullYear()}</p>
+</footer>
+
+
     </div>
   );
 };
